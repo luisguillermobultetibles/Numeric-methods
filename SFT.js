e1f9427f4c46ac05200e80f39e92b0b09339b541
luisguillermobultetibles <70673420+luisguillermobultetibles@users.noreply.github.com>
2023-02-02 09:21:01 -0600

    Add files via upload
    No logré poner a punto ni a DFT, FFT... pero este que logré satisface mis expectativas y está bastante rápido... tiene algunas funciones colaterales que pueden ser de utilidad.

---
+++ SFT.js
@@ -0,0 +1,98 @@
+// Transformación astronómica (Pipo '2023)
+// (Necesita poco entrenamiento, para predecir un comportamiento).
+function sFT(xvalues, yvalues, x) {
+    let residuo = (x, y) => {
+        return x - y * Math.floor(x / y);
+    }
+
+    function gcd(a, b) {
+        let eps = 0.00000000000001;
+        let result;
+        let [minFactor, maxFactor] = [Math.min(a, b), Math.max(a, b)];
+        // this trivial checks avoids div. by zero.
+        if (Math.abs(minFactor) <= eps) {
+            if (Math.abs(maxFactor) <= eps) {
+                result = Infinity;
+            } else {
+                result = maxFactor;
+            }
+        } else if (Math.abs(maxFactor) <= eps) {
+            if (Math.abs(minFactor) <= eps) {
+                result = Infinity;
+            } else {
+                result = minFactor;
+            }
+        } else {
+            result = maxFactor;
+            while (Math.abs(minFactor) > eps) {
+                maxFactor = result;
+                result = minFactor;
+                minFactor = residuo(maxFactor, minFactor);
+            }
+            if (minFactor > eps / 2) {
+                result = 1;
+            }
+        }
+        return result;
+    }
+
+    function arrayMcd(arr) {
+        let result;
+        arr.forEach((element, i) => {
+            if (i === 0) {
+                result = element;
+            } else {
+                result = gcd(result, element);
+            }
+        });
+        return result;
+    }
+
+    function divisibilidad(a, b) {
+        let eps = 0.00000000000001;
+        let result;
+        if (Math.abs(b) < eps) {
+            result = false;
+        } else {
+            result = Math.abs(residuo(a, b)) < eps;
+        }
+        return result;
+    }
+
+    function modpow(base, exp, m) {
+        if (m === 1) return 0;
+        if (exp === 0) return 1;
+        if (base === 0) return 0;
+        if (((base < 10) && (exp < 10) && (m < 10)) || (exp < 1)) {
+            return residuo(Math.pow(base, exp) , m);
+        }
+
+        if (exp > 50) {
+            let r = Math.sqrt(exp);
+            return modpow(modpow(base, r, m), r, m);
+        }
+
+        while (exp >= 2 && divisibilidad(exp, 2)) {
+            exp /= 2;
+            base = Math.pow(base, 2);
+        }
+        while (exp >= 3 && divisibilidad(exp, 3)) {
+            exp /= 3;
+            base = Math.pow(base, 3);
+        }
+        while (exp >= 5 && divisibilidad(exp, 5)) {
+            exp /= 5;
+            base = Math.pow(base, 5);
+        }
+
+        return residuo((base * modpow(base, exp - 1, m)) , m);
+    }
+
+    let mcd = arrayMcd(xvalues);
+    let result = 0;
+    for (let i = 0; i < yvalues.length; i++) {
+        let algo = modpow(2, Math.abs(x - xvalues[i]) / mcd - 1, 2);
+        result += yvalues[i] * Math.sin(Math.PI * algo);
+    }
+    return result;
+}
