02c14cc22ad2e42f69833e2114bfe358cf07eeb2
Luis Guillermo Bultet Ibles <70673420+luisguillermobultetibles@users.noreply.github.com>
2023-02-22 12:17:50 -0600

    Update toposort.js
    This is a new version, we find some error; the team abandon the idea... and go to the India, I follow, check this.

--- toposort.js
+++ toposort.js
@@ -1,19 +1,28 @@
-        // Topological sorting (Ordenar un conjunto de tareas dependientes una de otra en una posible secuencia de realización).
-        // let tasks = [{name: "tarea 2", dependencies : ["tarea 3", "tarea 4"]}, {name: "tarea 1", dependencies : ["tarea 2", "tarea 3"]}];
-        function solve(tasks) {
-            function depends(task1, task2) { // task2 depends of task1 ?
-                let vis = [];
-                function depVis(t1, t2) {
-                    if (vis.indexOf(t1.name) !== -1) {
-                        return 1;
-                    } else {
-                        vis.push(t1.name);
-                        return (task1.dependencies.some((element) => {
-                            return element.name === t2 && (element.dependencies.indexOf(t1) !== -1 || (depVis(t1, element.name) && depVis(element.name, t2)));
-                        })) ? 1 : -1;
-                    }
+    // Topological sorting (Ordenar un conjunto de tareas dependientes una de otra en una posible secuencia de realización).
+    // let tasks = [{name: "tarea 2", dependencies : ["tarea 3", "tarea 4"]}, {name: "tarea 1", dependencies : ["tarea 2", "tarea 3"]}];
+    function solve(tasks) {
+        function depends(task1, task2) {
+            let vis = [];
+            function depVis(t1, t2) {
+                let result;
+                if (t1 === t2 || tasks.some((element) => {
+                    return element.name === t1 && element.dependencies.indexOf(t2) !== 1
+                })) {
+                    result = 1;
+                } else if (vis.indexOf(t1) !== -1) {
+                    result = -1;
+                } else {
+                    vis.push(t1); // Clausuramos la entrada, si sobrevive: no aplica.
+                    result = tasks.some((element) => {
+                        result = depVis(element.name, t2) !== -1 && depVis(t1, element.name) !== -1;
+                    }) ? 1 : -1;
                 }
-                return depVis(task1, task2);
+                // console.log(`Dependencia circular de ${t1} de ${t2} = ${result}.`);
+                return result;
             }
-            return tasks.sort((task1, task2) => depends(task1, task2));
+            return depVis(task1.name, task2.name);
         }
+        return tasks.sort((task1, task2) => {
+            return depends(task1, task2) - depends(task2, task1)
+        });
+    }
