d0c4a449e22f0bd80ed6ccbc4b663926d7e034f8
Luis Guillermo Bultet Ibles <70673420+luisguillermobultetibles@users.noreply.github.com>
2023-02-25 13:33:24 -0600

    Update toposort.js
    Simplificando, se realiza un pre orden para minimizar las búsquedas, en reemplazo de visitados; y un post orden para organizar los resultados.

--- toposort.js
+++ toposort.js
@@ -1,28 +1,20 @@
-    // Topological sorting (Ordenar un conjunto de tareas dependientes una de otra en una posible secuencia de realización).
+        // Topological sorting (Ordenar un conjunto de tareas dependientes una de otra en una posible secuencia de realización).
     // let tasks = [{name: "tarea 2", dependencies : ["tarea 3", "tarea 4"]}, {name: "tarea 1", dependencies : ["tarea 2", "tarea 3"]}];
-    function solve(tasks) {
-        function depends(task1, task2) {
-            let vis = [];
-            function depVis(t1, t2) {
-                let result;
-                if (tasks.some((element) => {
-                    return element.name === t1 && element.dependencies.indexOf(t2) !== 1
-                })) {
-                    result = 1;
-                } else if (vis.indexOf(t1) !== -1) {
-                    result = -1;
-                } else {
-                    vis.push(t1); // Clausuramos la entrada, si sobrevive: no aplica.
-                    result = tasks.some((element) => {
-                        result = depVis(element.name, t2) !== -1 && depVis(t1, element.name) !== -1;
-                    }) ? 1 : -1;
-                }
-                // console.warn(`Dependencia circular de ${t1} de ${t2} = ${result}.`);
-                return result;
+    function topologicalSort(tasks) {
+        function depVis(task1, task2) {
+            if (!task1 || !task2 || task1.name > task2.name) {
+                return -1;
             }
-            return depVis(task1.name, task2.name);
+            if (tasks.some((element) =>
+                element.name === task1.name && element.dependencies.indexOf(task2.name) !== -1
+            )) {
+                return 1;
+            } else {
+                return task1.dependencies.some((element) => {
+                    let it = tasks.find((el) => element === el.name);
+                    return depVis(task1, it) === 1 && depVis(it, task2) === 1;
+                }) ? 1 : -1;
+            }
         }
-        return tasks.sort((task1, task2) => {
-            return depends(task1, task2) - depends(task2, task1)
-        });
+        return tasks.sort((a, b) => a.name < b.name ? 1 : -1).sort((task1, task2) => depVis(task1, task2));
     }
