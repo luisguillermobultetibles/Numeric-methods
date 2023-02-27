23a702d5e0958086a7feed81a6a774cbb692532f
Luis Guillermo Bultet Ibles <70673420+luisguillermobultetibles@users.noreply.github.com>
2023-02-27 09:32:08 -0600

    Update toposort.js
    Corrigiendo error de transcripción. versión estable.

--- toposort.js
+++ toposort.js
@@ -1,21 +1,15 @@
-        // Topological sorting (Ordenar un conjunto de tareas dependientes una de otra en una posible secuencia de realización).
+    // Topological sorting (Ordenar un conjunto de tareas dependientes una de otra en una posible secuencia de realización).
     // let tasks = [{name: "tarea 2", dependencies : ["tarea 3", "tarea 4"]}, {name: "tarea 1", dependencies : ["tarea 2", "tarea 3"]}];
+    const notDependant = -1;
+    const dependant = +1;
     function topologicalSort(tasks) {
         function depVis(task1, task2) {
-            if (!task1 || !task2 || task1.name > task2.name) {
-                return -1;
-            }
-            if (tasks.some((element) =>
-                element.name === task1.name && element.dependencies.indexOf(task2.name) !== -1
-            )) {
-                return 1;
-            } else {
-                return task1.dependencies.some((element) => {
-                    let it = tasks.find((el) => element === el.name);
-                    return depVis(task1, it) === 1 && depVis(it, task2) === 1;
-                }) ? 1 : -1;
-            }
+            if (!task1 || !task2 || task1.name > task2.name) return notDependant;
+            if (tasks.some((element) => element.name === task1.name && element.dependencies.indexOf(task2.name) !== -1)) return dependant;
+            else return task1.dependencies.some((element) => {
+                let it = tasks.find((el) => element === el.name);
+                return depVis(task1, it) === dependant && depVis(it, task2) === dependant;
+            }) ? dependant : notDependant;
         }
-        // optimize    
-        return tasks.sort((a, b) => a.name < b.name ? 1 : -1).sort((task1, task2) => depVis(task1, task2));
+        return tasks.sort((a, b) => a.name < b.name ? dependant : notDependant).sort((task1, task2) => depVis(task1, task2)).reverse();
     }
