1d620abbc7fb22f7804bef3e5457dc57ba894c07
Luis Guillermo Bultet Ibles <70673420+luisguillermobultetibles@users.noreply.github.com>
2023-03-22 08:50:47 -0400

    Add files via upload
    Updating daily functionalities...

--- Keyboard.js
+++ Keyboard.js
@@ -1,4 +1,5 @@
-import {WebSystemObject} from "./WebSystemObject.js";
+import {WebSystemObject} from './WebSystemObject.js';
+
 /*
    Clase para controlar el teclado...
    Usage (to attach key press and released):
@@ -19,164 +20,168 @@ import {WebSystemObject} from "./WebSystemObject.js";
                                    [80, () => {Game.togglePause()}]]);
 */
 export class Keyboard extends WebSystemObject {
-    static #lastKeyCode;
-    static #lastKey;
-    static #UpDownstates = [];
-    static #DownEvents = [];
-    static #UpEvents = [];
-    static #keyBACKSPACE = 8;
-    static #keyTAB = 9;
-    static #keyENTER = 13;
-    static #keySHIFT = 16;
-    static #keyCTRL = 17;
-    static #keyALT = 18;
-    static #keyPAUSE_BREAK = 19;
-    static #keyCAPS_LOCK = 20;
-    static #keyESCAPE = 27;
-    static #keySPACE = 32;
-    static #keyPAGE_UP = 33;
-    static #keyPAGE_DOWN = 34;
-    static #keyEND = 35;
-    static #keyHOME = 36;
-    static #keyLEFT_ARROW = 37;
-    static #keyUP_ARROW = 38;
-    static #keyRIGHT_ARROW = 39;
-    static #keyDOWN_ARROW = 40;
-    static #keyINSERT = 45;
-    static #keyDELETE = 46;
-    static #key0 = 48;
-    static #key1 = 49;
-    static #key2 = 50;
-    static #key3 = 51;
-    static #key4 = 52;
-    static #key5 = 53;
-    static #key6 = 54;
-    static #key7 = 55;
-    static #key8 = 56;
-    static #key9 = 57;
-    static #keya = 65;
-    static #keyb = 66;
-    static #keyc = 67;
-    static #keyd = 68;
-    static #keye = 69;
-    static #keyf = 70;
-    static #keyg = 71;
-    static #keyh = 72;
-    static #keyi = 73;
-    static #keyj = 74;
-    static #keyk = 75;
-    static #keyl = 76;
-    static #keym = 77;
-    static #keyn = 78;
-    static #keyo = 79;
-    static #keyp = 80;
-    static #keyq = 81;
-    static #keyr = 82;
-    static #keys = 83;
-    static #keyt = 84;
-    static #keyu = 85;
-    static #keyv = 86;
-    static #keyw = 87;
-    static #keyx = 88;
-    static #keyy = 89;
-    static #keyz = 90;
-    static #keyLET_WINDOW = 91;
-    static #keyRIGHT_WINDOW = 92;
-    static #keySELECT = 93;
-    static #keyNUMPAD_0 = 96;
-    static #keyNUMPAD_1 = 97;
-    static #keyNUMPAD_2 = 98;
-    static #keyNUMPAD_3 = 99;
-    static #keyNUMPAD_4 = 100;
-    static #keyNUMPAD_5 = 101;
-    static #keyNUMPAD_6 = 102;
-    static #keyNUMPAD_7 = 103;
-    static #keyNUMPAD_8 = 104;
-    static #keyNUMPAD_9 = 105;
-    static #keyMULTIPLY = 106;
-    static #keyADD = 107;
-    static #keySUBTRACT = 109;
-    static #keyDECIMAL_POINT = 110;
-    static #keyDIVIDE = 111;
-    static #keyF1 = 112;
-    static #keyF2 = 113;
-    static #keyF3 = 114;
-    static #keyF4 = 115;
-    static #keyF5 = 116;
-    static #keyF6 = 117;
-    static #keyF7 = 118;
-    static #keyF8 = 119;
-    static #keyF9 = 120;
-    static #keyF10 = 121;
-    static #keyF11 = 122;
-    static #keyF12 = 123;
-    static #keyNUM_LOCK = 144;
-    static #keySCROLL_LOCK = 145;
-    static #keySEMI_COLON = 186;
-    static #keyEQUAL_SIGN = 187;
-    static #keyCOMMA = 188;
-    static #keyDASH = 189;
-    static #keyPERIOD = 190;
-    static #keyFORWARD_SLASH = 191;
-    static #keyGRAVE_ACENT = 192;
-    static #keyOPEN_BRACKET = 219;
-    static #keyBACK_SLASH = 220;
-    static #keyCLOSE_BRACKET = 221;
-    static #keySINGLE_QUOTE = 222;
+  static keyCode;
+  static keyDown;
+  static UpDownstates = [];
+  static DownEvents = [];
+  static UpEvents = [];
+  static keyBACKSPACE = 8;
+  static keyTAB = 9;
+  static keyENTER = 13;
+  static keySHIFT = 16;
+  static keyCTRL = 17;
+  static keyALT = 18;
+  static keyPAUSE_BREAK = 19;
+  static keyCAPS_LOCK = 20;
+  static keyESCAPE = 27;
+  static keySPACE = 32;
+  static keyPAGE_UP = 33;
+  static keyPAGE_DOWN = 34;
+  static keyEND = 35;
+  static keyHOME = 36;
+  static keyLEFT_ARROW = 37;
+  static keyUP_ARROW = 38;
+  static keyRIGHT_ARROW = 39;
+  static keyDOWN_ARROW = 40;
+  static keyINSERT = 45;
+  static keyDELETE = 46;
+  static key0 = 48;
+  static key1 = 49;
+  static key2 = 50;
+  static key3 = 51;
+  static key4 = 52;
+  static key5 = 53;
+  static key6 = 54;
+  static key7 = 55;
+  static key8 = 56;
+  static key9 = 57;
+  static keya = 65;
+  static keyb = 66;
+  static keyc = 67;
+  static keyd = 68;
+  static keye = 69;
+  static keyf = 70;
+  static keyg = 71;
+  static keyh = 72;
+  static keyi = 73;
+  static keyj = 74;
+  static keyk = 75;
+  static keyl = 76;
+  static keym = 77;
+  static keyn = 78;
+  static keyo = 79;
+  static keyp = 80;
+  static keyq = 81;
+  static keyr = 82;
+  static keys = 83;
+  static keyt = 84;
+  static keyu = 85;
+  static keyv = 86;
+  static keyw = 87;
+  static keyx = 88;
+  static keyy = 89;
+  static keyz = 90;
+  static keyLEFT_WINDOW = 91;
+  static keyRIGHT_WINDOW = 92;
+  static keySELECT = 93;
+  static keyNUMPAD_0 = 96;
+  static keyNUMPAD_1 = 97;
+  static keyNUMPAD_2 = 98;
+  static keyNUMPAD_3 = 99;
+  static keyNUMPAD_4 = 100;
+  static keyNUMPAD_5 = 101;
+  static keyNUMPAD_6 = 102;
+  static keyNUMPAD_7 = 103;
+  static keyNUMPAD_8 = 104;
+  static keyNUMPAD_9 = 105;
+  static keyMULTIPLY = 106;
+  static keyADD = 107;
+  static keySUBTRACT = 109;
+  static keyDECIMAL_POINT = 110;
+  static keyDIVIDE = 111;
+  static keyF1 = 112;
+  static keyF2 = 113;
+  static keyF3 = 114;
+  static keyF4 = 115;
+  static keyF5 = 116;
+  static keyF6 = 117;
+  static keyF7 = 118;
+  static keyF8 = 119;
+  static keyF9 = 120;
+  static keyF10 = 121;
+  static keyF11 = 122;
+  static keyF12 = 123;
+  static keyNUM_LOCK = 144;
+  static keySCROLL_LOCK = 145;
+  static keySEMI_COLON = 186;
+  static keyEQUAL_SIGN = 187;
+  static keyCOMMA = 188;
+  static keyDASH = 189;
+  static keyPERIOD = 190;
+  static keyFORWARD_SLASH = 191;
+  static keyGRAVE_ACENT = 192;
+  static keyOPEN_BRACKET = 219;
+  static keyBACK_SLASH = 220;
+  static keyCLOSE_BRACKET = 221;
+  static keySINGLE_QUOTE = 222;
 
-    constructor(keyCode, onPressed, onReleased) {
-        if (arguments.length === 1 && arguments[0] instanceof Array) {
-            // Array of events
-            arguments[0].forEach((attach) => new Keyboard(...attach));
-        } else {
-            // The normal
-            if (Keyboard.#DownEvents.length === 0 && Keyboard.#UpEvents.length === 0) {
-                window.addEventListener('keypress', Keyboard.#keyPress, false);
-                window.addEventListener('keyup', Keyboard.#keyUpEvent, false);
-                window.addEventListener('keydown', Keyboard.#keyDownEvent, false);
-            }
-            this.keyCode = keyCode;
-            this.onPressed = Object(onPressed);
-            Keyboard.#DownEvents[this.keyCode] = this.onPressed;
-            if (onReleased) {
-                this.onReleased = Object(onReleased);
-                Keyboard.#UpEvents[this.keyCode] = this.onReleased;
-            }
-        }
+  static upDownStates = [];
+  static #upEvents = [];
+  static #downEvents = [];
+
+  constructor(keyCode, onPressed, onReleased) {
+    super();
+    if (arguments.length === 1 && arguments[0] instanceof Array) {
+      // Array of events
+      arguments[0].forEach((attach) => new Keyboard(...attach));
+    } else {
+      // The normal
+      if (Keyboard.#downEvents.length === 0 && Keyboard.#upEvents.length === 0) {
+        // window.addEventListener('keypress', Keyboard.#keyPress, false);
+        window.addEventListener('keyup', Keyboard.#keyUpEvent, false);
+        window.addEventListener('keydown', Keyboard.#keyDownEvent, false);
+      }
+      this.keyCode = keyCode;
+      this.onPressed = Object(onPressed);
+      Keyboard.#downEvents[this.keyCode] = this.onPressed;
+      if (onReleased) {
+        this.onReleased = Object(onReleased);
+        Keyboard.#upEvents[this.keyCode] = this.onReleased;
+      }
     }
+  }
 
-    static #keyPress(e) {
-        Keyboard.#lastKeyCode = e.keyCode;
-        Keyboard.#lastKey = String.fromCharCode(Keyboard.#lastKeyCode);
+  get keyChar() {
+    return String.fromCharCode(Keyboard.key);
+  }
+
+  static #keyDownEvent = (e) => {
+    Keyboard.keyDown = e.keyCode ? e.keyCode : 0;
+    Keyboard.upDownStates[Keyboard.keyDown] = true;
+    if (Keyboard.#downEvents[Keyboard.keyCode]) {
+      Keyboard.#downEvents[Keyboard.keyCode](e);
     }
+  };
 
-    static #keyDownEvent = (e) => {
-        const deco = e.keyCode ? e.keyCode : Keyboard.#lastKeyCode;
-        Keyboard.#UpDownstates[deco] = true;
-        if (Keyboard.#DownEvents[deco]) {
-            Keyboard.#DownEvents[deco](e);
-        }
-    };
-
-    static #keyUpEvent = (e) => {
-        const deco = e.keyCode ? e.keyCode : Keyboard.#lastKeyCode;
-        Keyboard.#UpDownstates[deco] = false;
-        if (Keyboard.#UpEvents[deco]) {
-            Keyboard.#UpEvents[deco](e);
-        }
-    };
-
-    free() {
-        if (this.onPressed) {
-            Keyboard.#DownEvents = Keyboard.#DownEvents.filter((event, Index) => Index !== this.keyCode);
-        }
-        if (this.onReleased) {
-            Keyboard.#DownEvents = Keyboard.#UpEvents.filter((event, Index) => Index !== this.keyCode);
-        }
-        if (Keyboard.#DownEvents.length === 0 && Keyboard.#UpEvents.length === 0) {
-            window.removeEventListener('keypress', Keyboard.#keyPress);
-            window.removeEventListener('keyup', Keyboard.#keyUpEvent);
-            window.removeEventListener('keydown', Keyboard.#keyDownEvent);
-        }
+  static #keyUpEvent = (e) => {
+    Keyboard.keyDown = e.keyCode ? e.keyCode : 0;
+    Keyboard.upDownStates[Keyboard.keyDown] = false;
+    if (Keyboard.#upEvents[Keyboard.keyCode]) {
+      Keyboard.#upEvents[Keyboard.keyCode](e);
     }
+  };
+
+  free() {
+    if (this.onPressed) {
+      Keyboard.#downEvents = Keyboard.#downEvents.filter((event, Index) => Index !== Keyboard.keyCode);
+    }
+    if (this.onReleased) {
+      Keyboard.#downEvents = Keyboard.#upEvents.filter((event, Index) => Index !== Keyboard.keyCode);
+    }
+    if (Keyboard.#downEvents.length === 0 && Keyboard.#upEvents.length === 0) {
+      // window.removeEventListener('keypress', Keyboard.#keyPress);
+      window.removeEventListener('keyup', Keyboard.#keyUpEvent);
+      window.removeEventListener('keydown', Keyboard.#keyDownEvent);
+    }
+  }
 }
--- Mouse.js
+++ Mouse.js
@@ -1,88 +1,174 @@
-import {WebSystemObject} from "./system/WebSystemObject.js";
+import {WebSystemObject} from './system/WebSystemObject.js';
+import {Keyboard} from './Keyboard';
 
 // Clase para controlar el ratón...
 export class Mouse extends WebSystemObject {
-    static #isMouseDown = false;
-    static x = 0;
-    static y = 0;
+  #isMouseDown = false;
+  target = window;
+  position = new Vector(0, 0);
+  #lastPosition = new Vector(x, y);
+  direction = new Vector(0, 0);
+  leftButton = false;
+  midButton = false;
+  rightButton = false; // Context menu, use in conjunction with: document.addEventListener("contextmenu", function(e){ e.preventDefault() });
+  fourthButton = false; // Browserback
+  fifthButton = false; // Browser forward
+  shift = Keyboard.upDownStates[Keyboard.keySHIFT];
+  alt = Keyboard.upDownStates[Keyboard.keyALT];
+  ctrlKey = Keyboard.upDownStates[Keyboard.keyCTRL];
+  #DownEvents = [];
+  #UpEvents = [];
+  #MoveEvents = [];
+  #WheelEvents = [];
 
-    static #DownEvents = [];
-    static #UpEvents = [];
-    static #MoveEvents = [];
-    static #WheelEvents = [];
+  static #mouseDown(e) {
+    this.#isMouseDown = true;
+    document.title = `Mouse abajo ${e}: estado ${this.#isMouseDown ? 'abajo' : 'arriba'} en (${this.position.x}, ${this.position.y}).`;
+    document.body.style.cursor = 'grabbing';
 
-    static #mouseDown(e) {
-        Mouse.#isMouseDown = true;
-        document.title = `Mouse abajo ${e}: estado ${Mouse.#isMouseDown ? 'abajo' : 'arriba'} en (${Mouse.x}, ${Mouse.y}).`;
-        document.body.style.cursor = 'grabbing';
-        Mouse.#DownEvents.forEach((event) => event(e));
-    };
-
-    static #mouseUp(e) {
-        Mouse.#isMouseDown = false;
-        document.title = `Mouse arriba ${e}: estado ${Mouse.#isMouseDown ? 'abajo' : 'arriba'} en (${Mouse.x}, ${Mouse.y}).`;
-        document.body.style.cursor = 'grab';
-        Mouse.#UpEvents.forEach((event) => event(e));
-    };
-
-    static #mouseMove(e) {
-        Mouse.x = e.clientX;
-        Mouse.y = e.clientY;
-        document.title = `Mouse moviéndose ${e}: estado ${Mouse.#isMouseDown ? 'abajo' : 'arriba'} en (${Mouse.x}, ${Mouse.y}).`;
-        Mouse.#MoveEvents.forEach((event) => event(e));
-    };
-
-    static #mouseWheel(e) {
-        Mouse.x = e.clientX;
-        Mouse.y = e.clientY;
-        document.title = `Mouse scroll ${e}: estado ${Mouse.#isMouseDown ? 'abajo' : 'arriba'} en (${Mouse.x}, ${Mouse.y}).`;
-        Mouse.#WheelEvents.forEach((event) => event(e));
-    };
-
-    constructor(onMouseDown, onMouseMove, onMouseUp, onMouseWheel) {
-        super();
-        if (Mouse.#DownEvents.length === 0 && Mouse.#UpEvents.length === 0 && Mouse.#MoveEvents.length === 0) {
-            window.addEventListener('mousedown', Mouse.#mouseDown, false);
-            window.addEventListener('mouseup', Mouse.#mouseUp, false);
-            window.addEventListener('mousemove', Mouse.#mouseMove, false);
-            window.addEventListener('wheel', Mouse.#mouseWheel, false);
-        }
-        this.downEventsPosition = -1;
-        this.upEventsPosition = -1;
-        this.moveEventsPosition = -1;
-        if (onMouseDown) {
-            Mouse.#DownEvents.push(onMouseDown);
-            this.downEventsPosition = Mouse.#DownEvents.length - 1;
-        }
-        if (onMouseUp) {
-            Mouse.#UpEvents.push(onMouseUp);
-            this.upEventsPosition = Mouse.#UpEvents.length - 1;
-        }
-        if (onMouseMove) {
-            Mouse.#MoveEvents.push(onMouseMove);
-            this.moveEventsPosition = Mouse.#MoveEvents.length - 1;
-        }
-        if (onMouseWheel) {
-            Mouse.#WheelEvents.push(onMouseWheel);
-            this.wheelEventsPosition = Mouse.#WheelEvents.length - 1;
-        }
+    if (e.which === 1 || e.button === 0) {
+      this.leftButton = true;
+    } else if (e.which === 2 || e.button === 1) {
+      this.midButton = true;
+    } else if (e.which === 3 || e.button === 2) {
+      this.rightButton = true;
+    } else if (e.which === 4 || e.button === 3) {
+      this.fourthButton = true;
+    } else if (e.which === 5 || e.button === 4) {
+      this.fifthButton = true;
     }
 
-    get down() { // don't care if move all around
-        return Mouse.#isMouseDown;
+    this.shift = e.shiftKey;
+    this.alt = e.altKey;
+    this.ctrlKey = e.ctrlKey;
+
+    this.#DownEvents.forEach((event) => event(e));
+  };
+
+  static #mouseUp(e) {
+    this.#isMouseDown = false;
+    document.title = `Mouse arriba ${e}: estado ${this.#isMouseDown ? 'abajo' : 'arriba'} en (${this.position.x}, ${this.position.y}).`;
+    document.body.style.cursor = 'grab';
+
+    if (e.which === 1 || e.button === 0) {
+      this.leftButton = false;
+    } else if (e.which === 2 || e.button === 1) {
+      this.midButton = false;
+    } else if (e.which === 3 || e.button === 2) {
+      this.rightButton = false;
+    } else if (e.which === 4 || e.button === 3) {
+      this.fourthButton = false;
+    } else if (e.which === 5 || e.button === 4) {
+      this.fifthButton = false;
     }
 
-    free() {
-        Mouse.#DownEvents = Mouse.#DownEvents.filter((event, Index) => Index !== this.downEventsPosition);
-        Mouse.#UpEvents = Mouse.#UpEvents.filter((event, Index) => Index !== this.upEventsPosition);
-        Mouse.#MoveEvents = Mouse.#MoveEvents.filter((event, Index) => Index !== this.moveEventsPosition);
-        Mouse.#WheelEvents = Mouse.#WheelEvents.filter((event, Index) => Index !== this.wheelEventsPosition);
+    this.#UpEvents.forEach((event) => event(e));
+  };
 
-        if (Mouse.#DownEvents.length === 0 & Mouse.#UpEvents.length === 0 && Mouse.#MoveEvents.length === 0) {
-            window.removeEventListener('mousedown', Mouse.#mouseDown, false);
-            window.removeEventListener('mouseup', Mouse.#mouseUp, false);
-            window.removeEventListener('mousemove', Mouse.#mouseMove, false);
-            window.removeEventListener('wheel', Mouse.#mouseMove, false);
-        }
+  static #mouseMove(e) {
+    this.position.x = e.clientX;
+    this.position.y = e.clientY;
+
+    [this.direction.x, this.direction.y] = [x - this.#lastPosition.x, y - this.#lastPosition.y];
+    [this.#lastPosition.x, this.#lastPosition.y] = [x, y];
+
+    document.title = `Mouse moviéndose ${e}: estado ${this.#isMouseDown ? 'abajo' : 'arriba'} en (${this.position.x}, ${this.position.y}).`;
+    this.#MoveEvents.forEach((event) => event(e));
+  };
+
+  static #mouseWheel(e) {
+    this.position.x = e.clientX;
+    this.position.y = e.clientY;
+
+    let dx = Math.sign(e.deltaX);
+    let dy = Math.sign(e.deltaY);
+    let dz = Math.sign(e.deltaZ);
+
+    let units;
+
+    if (e.deltaMode === 0) {
+      units = 'pixels';
+    } else if (e.deltaMode === 1) {
+      units = 'lines';
+    } else if (e.deltaMode === 2) {
+      units = 'pages';
     }
+
+    document.title = `Mouse wheel ${Math.sqrt(dx + dy + dz)} ${units}, ${dx > 0 ? 'hacia derecha.' : 'hacia izquierda.'}.`;
+    document.title += ` ${dy > 0 ? 'hacia abajo.' : 'hacia arriba.'}.`;
+    document.title += ` ${dz > 0 ? 'hacia arriba.' : 'hacia abajo.'}.`;
+    this.#WheelEvents.forEach((event) => event(e));
+
+  };
+
+  /*
+
+  function zoom(event) {
+  event.preventDefault();
+
+  scale += event.deltaY * -0.01;
+
+  // Restrict scale
+  scale = Math.min(Math.max(0.125, scale), 4);
+
+  // Apply scale transform
+  el.style.transform = `scale(${scale})`;
+}
+
+Incluir además mouseenter, mouseleave, mouseout (se va de los hijos) y mouseover
+
+
+  */
+
+  constructor(onMouseDown, onMouseMove, onMouseUp, onMouseWheel, target = window) {
+    super();
+
+    if (this.#DownEvents.length === 0 && this.#UpEvents.length === 0 && this.#MoveEvents.length === 0) {
+      if (target) {
+        this.target = target;
+      }
+      this.target.addEventListener('mousedown', this.#mouseDown, false);
+      this.target.addEventListener('mouseup', this.#mouseUp, false);
+      this.target.addEventListener('mousemove', this.#mouseMove, false);
+      this.target.addEventListener('wheel', this.#mouseWheel, false);
+    }
+    this.downEventsPosition = -1;
+    this.upEventsPosition = -1;
+    this.moveEventsPosition = -1;
+
+    if (onMouseDown) {
+      this.#DownEvents.push(onMouseDown);
+      this.downEventsPosition = this.#DownEvents.length - 1;
+    }
+    if (onMouseUp) {
+      this.#UpEvents.push(onMouseUp);
+      this.upEventsPosition = this.#UpEvents.length - 1;
+    }
+    if (onMouseMove) {
+      this.#MoveEvents.push(onMouseMove);
+      this.moveEventsPosition = this.#MoveEvents.length - 1;
+    }
+    if (onMouseWheel) {
+      this.#WheelEvents.push(onMouseWheel);
+      this.wheelEventsPosition = this.#WheelEvents.length - 1;
+    }
+  }
+
+  get down() { // don't care if move all around
+    return this.#isMouseDown;
+  }
+
+  free() {
+    this.#DownEvents = this.#DownEvents.filter((event, Index) => Index !== this.downEventsPosition);
+    this.#UpEvents = this.#UpEvents.filter((event, Index) => Index !== this.upEventsPosition);
+    this.#MoveEvents = this.#MoveEvents.filter((event, Index) => Index !== this.moveEventsPosition);
+    this.#WheelEvents = this.#WheelEvents.filter((event, Index) => Index !== this.wheelEventsPosition);
+
+    if (this.#DownEvents.length === 0 & this.#UpEvents.length === 0 && this.#MoveEvents.length === 0 && this.target) {
+      this.target.removeEventListener('mousedown', this.#mouseDown, false);
+      this.target.removeEventListener('mouseup', this.#mouseUp, false);
+      this.target.removeEventListener('mousemove', this.#mouseMove, false);
+      this.target.removeEventListener('wheel', this.#mouseMove, false);
+    }
+  }
 }
